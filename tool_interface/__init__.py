2d26caec07f03c6b0310ad229127b08e1683bdd6
mmaelicke <mirko@hydrocode.de>
2025-05-14 10:53:04 +0200

    Update versioning strategy in pyproject.toml and __init__.py to use dynamic version retrieval from importlib.metadata

--- tool_interface/__init__.py
+++ tool_interface/__init__.py
@@ -2,4 +2,10 @@
 tool_interface - Main Python package to access the 3DTrees backend from tool containers
 """
 
-__version__ = "0.1.0"
+from importlib.metadata import version
+
+try:
+    __version__ = version("tool_interface")
+except Exception:  # pragma: no cover
+    # If the package is not installed, use a default version
+    __version__ = "0.0.0"
